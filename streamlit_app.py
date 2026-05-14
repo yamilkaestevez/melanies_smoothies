b8ebbb6e2ab3411cd6a491a2da2c73da80f810c7
yamilkaestevez <78061403+yamilkaestevez@users.noreply.github.com>
2026-05-14 10:43:12 -0400

    Update streamlit_app.py
    Delete Session code

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,6 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
-
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
