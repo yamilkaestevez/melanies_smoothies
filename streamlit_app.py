7891471e89f32844e09b6b27ac51144c64ca3077
yamilkaestevez <78061403+yamilkaestevez@users.noreply.github.com>
2026-05-14 10:59:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ ingredients_list = st.multiselect(
 
 if ingredients_list:
 
-    ingredients_string = ''
+    ingredients_string = '' 
 
     for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen + ' '
